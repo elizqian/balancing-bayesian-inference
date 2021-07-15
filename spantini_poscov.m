13789674cb0150224632f3cc301496a3b4a9852a
JemimaT <jemima.tabeart@ed.ac.uk>
2021-07-15 17:18:08 +0100

    Update to projection operator

--- spantini_poscov.m
+++ spantini_poscov.m
@@ -1,4 +1,4 @@
-function [Gpos, del2,info] = spantini_poscov(LH,L_pr,r_vals,method)
+function [Gpos, del2,Pi,info] = spantini_poscov(LH,L_pr,r_vals,method)
 
 if nargin == 3
     method = 'svd';
@@ -15,6 +15,7 @@ end
 
 d = size(H,1);
 Gpos = zeros(d,d,length(r_vals)+1);
+Pi = zeros(d,d,length(r_vals)+1);
 
 % true posterior cov
 prec = inv(Gamma_pr);
@@ -30,14 +31,18 @@ switch method
             What(:,i) = What(:,i) / sqrt(What(:,i)'*prec*What(:,i));
         end
         del2 = diag(D(a,a));
+        Vhat = prec*What;
     case 'svd'
         % low rank SVD calc
         [myV,myS,myW] = svd(LH'*L_pr);
         What = L_pr*myW;
+        Vhat = prec*What;
         del2 = diag(myS).^2;
         info.V = myV;
         info.S = myS;
         info.W = myW;
+        
+        
 end
 
 % compute low-rank updates for all specified ranks in r_vals
@@ -45,4 +50,6 @@ for rr = 1:length(r_vals)
     r = r_vals(rr);
     KKT = What(:,1:r)*diag(del2(1:r)./(1+del2(1:r)))*What(:,1:r)';
     Gpos(:,:,rr) = Gamma_pr - KKT;
+    % Compute projector
+    Pi(:,:,rr) = Vhat(:,1:rr)*What(:,1:rr)';
 end
